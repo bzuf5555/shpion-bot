06a26c6c5d4ef927905ff590bd4e1c578a688667
bzuf5555 <bzuf5555@gmail.com>
2026-05-03 13:21:19 +0500

    Replace cars images with 10 real car photos

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ DATABASE_URL: str = os.getenv("DATABASE_URL", "")
 CATEGORIES: list[str] = ["Cars", "Watches", "Jobs", "Bloggers", "18+ Actress"]
 
 CATEGORY_IMAGES: dict[str, list[str]] = {
-    "Cars":        ["images/cars_1.jpg",     "images/cars_2.jpg",     "images/cars_3.jpg"],
+    "Cars":        [f"images/cars_{i}.jpg" for i in range(1, 11)],
     "Watches":     ["images/watches_1.jpg",  "images/watches_2.jpg",  "images/watches_3.jpg"],
     "Jobs":        ["images/jobs_1.jpg",     "images/jobs_2.jpg",     "images/jobs_3.jpg"],
     "Bloggers":    ["images/bloggers_1.jpg", "images/bloggers_2.jpg", "images/bloggers_3.jpg"],
